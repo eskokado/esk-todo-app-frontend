3d098c8f7e0d19de95f8f3458054db250bbc8c16
Edson Shideki Kokado <eskokado@gmail.com>
2020-01-10 10:33:06 -0300

    Evento Adicionar

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,11 +4,20 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.handledAdd = this.handledAdd.bind(this)
+    }
+
+    handledAdd() {
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
-                <TodoForm />
+                <TodoForm handledAdd={this.handledAdd}/>
                 <TodoList />
             </div>
         )
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -10,7 +10,9 @@ export default props => (
             />
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'/>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handledAdd}
+            />
         </Grid>
     </div>
 )
