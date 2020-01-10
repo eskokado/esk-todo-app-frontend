cda3d157881e25bfd85a71d72047d1a3fb40c166
Edson Shideki Kokado <eskokado@gmail.com>
2020-01-10 10:49:23 -0300

    Evento onChange, bug onClick

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -6,18 +6,28 @@ import TodoList from './todoList'
 export default class Todo extends Component {
     constructor(props) {
         super(props)
-        this.handledAdd = this.handledAdd.bind(this)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
     }
 
-    handledAdd() {
-        console.log(this)
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
     }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
-                <TodoForm handledAdd={this.handledAdd}/>
+                <TodoForm handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                    description={this.state.description}
+                />
                 <TodoList />
             </div>
         )
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -7,11 +7,13 @@ export default props => (
         <Grid cols='12 9 10'>
             <input type="text" id="description" className="form-control"
                 placeholder='Adicione uma tarefa'
+                onChange={props.handleChange}
+                value={props.description}
             />
         </Grid>
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus'
-                onClick={props.handledAdd}
+                onClick={props.handleAdd}
             />
         </Grid>
     </div>
